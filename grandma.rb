0200e4185446373100f6700eac1f8b32c2b5af4b
Alicia Reeves <reevesalic@gmail.com>
2020-06-12 17:49:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == lowercase
+  if speak_to_grandma(phrase) == lowercase
     puts "HUH? SPEAK UP, SONNY!"
   elsif pharse == uppercase
     puts "NO, NOT SINCE 1938!"
