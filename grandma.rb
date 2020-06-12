9f066a44fa8efedba370e45b2c6f62fbea4587c2
Alicia Reeves <reevesalic@gmail.com>
2020-06-12 17:36:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if
-    phrase == lowercase
+    speak_to_grandma(phrase) == lowercase
     puts "HUH? SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
