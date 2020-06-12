8f7af1a4458ee8bc8e92df59383fbda6b371d498
Alicia Reeves <reevesalic@gmail.com>
2020-06-12 17:57:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-
+  else
    return "HUH? SPEAK UP, SONNY!"
   end
 end
